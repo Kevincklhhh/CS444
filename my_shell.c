2481d10f64dc83653b91f181b2b4a92ff5ec2a4d
Kevincklhhh <Kevin_ckl@outlook.com>
2023-02-19 22:17:13 -0500

    warning suppressed

--- my_shell.c
+++ my_shell.c
@@ -3,7 +3,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-
+#include <signal.h>
+#include <sys/wait.h>
 #define MAX_INPUT_SIZE 1024
 #define MAX_TOKEN_SIZE 64
 #define MAX_NUM_TOKENS 64
